8155ed20345a6e2af106fb9ef50e7082efb077fb
Valentin Barral <valentin.barral@gtec.udc.es>
2018-06-01 10:53:29 +0200

    Added new script to record sensor data from robot

---
+++ record_robot_sensors.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+topics_to_record=("/gtec/vr/pos" "/raspicam_node/image/compressed" "/gtec/toa/anchors" "/gtec/toa/ranging")
+
+if [ "$#" -ne 3 ]; then
+    echo "Use: ./record_robot_sensors basename num_reps rep_duration"
+    exit 1
+fi
+
+filename=$1
+num_reps=$2
+logDuration=$3
+
+num_reps=$(expr ${num_reps} - 1)
+
+for runIndex in {0..${num_reps}}
+do
+  current_date=$(date +%d%m%y_%H%M%S)
+  newRecordFilename="${filename}_${runIndex}_${current_date}"
+  rosbag record ${topics_to_record} --output-name=${newRecordFilename} -q --duration=${logDuration}
+done
+
